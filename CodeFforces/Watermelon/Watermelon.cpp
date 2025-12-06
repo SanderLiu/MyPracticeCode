2061f5a72eeed27c9dc6c19a7dffa5e81f44a627
sanderliu <xjtu_liusiyang@163.com>
2025-12-07 01:08:27 +0800

    add codeforces 4A watermelon

---
+++ CodeFforces/Watermelon/Watermelon.cpp
@@ -0,0 +1,19 @@
+/**
+ * Codeforces 
+ * 4A Watermelon
+ * 
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int w = 0;
+    cin >> w;
+    if (w > 2 && w % 2 == 0) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
